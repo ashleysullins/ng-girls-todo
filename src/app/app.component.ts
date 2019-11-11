4e027acad7dc74edaf5c079013508501a39af28a
Ashley Sullins <ashley.sullins@intouchsol.com>
2019-11-11 13:57:36 -0600

    Add angular tutorial to github pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+          <h1 class="app-title"> Welcome! </h1>
+          <app-list-manager></app-list-manager>
+          `,
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
